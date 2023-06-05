78cc661e2d5314bd4fae0a541925854e7d79fd45
Zotoff <paulzotoff@gmail.com>
2023-06-05 19:00:36 +0300

    Attempt 2

--- src/main/java/lang/print/gaps/task2/NamingConvention.java
+++ src/main/java/lang/print/gaps/task2/NamingConvention.java
@@ -3,13 +3,13 @@ package lang.print.gaps.task2;
 public class NamingConvention {
     public static void main(String[] args) {
         int _variable = 10;
-        int variableOne = 1;
-        int variableTwo = 2;
-        int variableThree = 3;
-        final int variableFour = 4;
-        final int variableFive = 5;
-        int variableSix = 6;
-        int variableSeven = 7;
-        int variableEight = 8;
+        int variable_One = 1;
+        int variable_Two = 2;
+        int variable_three = 3;
+        final int variable_four = 4;
+        final int variable_five = 5;
+        int variable_six = 6;
+        int variable_seven = 7;
+        int variable_eight = 8;
     }
 }
--- src/main/java/lang/print/gaps/task2/advancedNamingConvention.java
+++ src/main/java/lang/print/gaps/task2/advancedNamingConvention.java
@@ -3,14 +3,14 @@ package lang.print.gaps.task2;
 public class advancedNamingConvention {
     private static final int adult_age = 18;
 
-    private int age;
+    private int AGE;
     private int phoneNumber;
 
-    void callToFriend() {
+    void call_to_friend() {
         //some code
     }
 
-    void callByNumber(int number) {
+    void call_by_number(int phoneNumber) {
         //some code
     }
 }
